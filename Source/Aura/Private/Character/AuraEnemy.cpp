33ddca92bd6dcce71e42ced5ce95ffde51b852ef
Berkay_Şentürk <berkay0senturk9@gmail.com>
2024-05-08 17:56:35 +0300

    highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,25 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
 	UE_LOG(LogTemp, Warning, TEXT("highlighted"))
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(Custom_Depth_Red);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(Custom_Depth_Red);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	UE_LOG(LogTemp, Warning, TEXT("unhighlighted"))
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
