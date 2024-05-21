5a598856a77ef9050b6baeaf7a4abbd1f013f60b
Berkay_Şentürk <berkay0senturk9@gmail.com>
2024-05-21 11:59:41 +0300

    attribute menu widget controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// No-Copyright
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValue()
+{
+	
+}
