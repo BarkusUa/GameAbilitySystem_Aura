c2d7fe4a5df79261e236574d8ea77f13d527c832
Berkay_Şentürk <berkay0senturk9@gmail.com>
2024-07-19 12:10:15 +0300

    adding xp to the player state

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -7,6 +7,8 @@
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32, /*StatValue*/)
+
 class UAttributeSet;
 /**
  * 
@@ -21,10 +23,19 @@ public:
 	
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
-	
 	UAttributeSet* GetAttributeSet() const { return AttributeSet;}
 
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+
 	FORCEINLINE int32 GetPlayerLevel() const {return Level;}
+	FORCEINLINE int32 GetXP() const {return XP;}
+
+	void AddToXP(int32 InXP);
+	void AddToLevel(int32 InLevel);
+	
+	void SetXP(int32 InXP);
+	void SetLevel(int32 InLevel);
 
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -38,6 +49,12 @@ private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_Level)
 	int32 Level = 1;
 
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_XP)
+	int32 XP  = 1;
+
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 };
