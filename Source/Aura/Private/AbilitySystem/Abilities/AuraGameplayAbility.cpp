b66db151f7de9cf426f96312a3b2628d013619d2
Berkay_Şentürk <berkay0senturk9@gmail.com>
2024-07-22 09:53:55 +0300

    spell description

--- Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
@@ -3,3 +3,16 @@
 
 #include "AbilitySystem/Abilities/AuraGameplayAbility.h"
 
+FString UAuraGameplayAbility::GetDescription(int32 Level)
+{
+	return FString::Printf(TEXT("<Default>%s, </><Level>%d</>"), L"Default Ability Name - loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum loremIpsum", Level);
+}
+
+FString UAuraGameplayAbility::GetNextLevelDescription(int32 Level)
+{
+	return FString::Printf(TEXT("<Default>Next Level: </><Level>%d</> /n<Default>Causes momre damage</>"), Level);
+}
+
+FString UAuraGameplayAbility::GetLockedDescription(int32 Level)
+{return FString::Printf(TEXT("<Default>Spell Locked until Level: %d</>"), Level);
+}
