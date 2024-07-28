fa330b03efe9bf0e7b641a632502818fd01dc7ac
Berkay_Şentürk <berkay0senturk9@gmail.com>
2024-07-28 16:10:48 +0300

    fireball addedddd

---
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -0,0 +1,15 @@
+// No-Copyright
+
+
+#include "Actor/AuraFireBall.h"
+
+void AAuraFireBall::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OnOverlappedComponent, AActor* OtherActor,
+                                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -35,7 +35,7 @@ protected:
 	virtual void Destroyed() override;
 	virtual void BeginPlay() override;
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OnOverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OnOverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:
 
