7b82d11efc8b3631a298e325dc0f6bbcfced46dd
Berkay_Şentürk <berkay0senturk9@gmail.com>
2024-07-29 15:35:30 +0300

    it aint workin but

---
+++ Source/Aura/Private/UI/HUD/LoadScreenHUD.cpp
@@ -0,0 +1,19 @@
+// No-Copyright
+
+
+#include "UI/HUD/LoadScreenHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UI/ViewModel/MVVM_LoadScreen.h"
+#include "UI/Widget/LoadScreenWidget.h"
+
+void ALoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LoadScreenViewModel = NewObject<UMVVM_LoadScreen>(this, LoadScreenViewModelClass);
+
+	LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
+	LoadScreenWidget->AddToViewport();
+	
+}
---
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -0,0 +1,5 @@
+// No-Copyright
+
+
+#include "UI/ViewModel/MVVM_LoadScreen.h"
+
